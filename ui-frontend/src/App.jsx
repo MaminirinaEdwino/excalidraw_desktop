323eb79b665ebcfe6cdce9d0c1672f0d1bec8065
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-18 15:54:56 +0300

    fix: excalidraw style

--- ui-frontend/src/App.jsx
+++ ui-frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { Excalidraw } from "@excalidraw/excalidraw";
+import "@excalidraw/excalidraw/index.css";
 
 function App() {
  
   return (
     <>
-      <div>
+      <div style={{ height: "100vh", width: "100vw" }} className="custom-styles">
         <Excalidraw/>
       </div>
     </>
