ae5487121f793a76f36f11d6e074ede9bba07a8b
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-18 15:39:18 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MaminirinaEdwino/excalidraw_desktop
+
+go 1.25.7
---
+++ ui-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Excalidraw } from "@excalidraw/excalidraw";
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <Excalidraw/>
+      </div>
+    </>
+  )
+}
+
+export default App
